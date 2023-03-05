ac44bb24ec0d76590bcab4779a22c849ab4e2e3b
Lucas Coelho <llucascoelho33@gmail.com>
2023-03-05 11:48:59 -0300

    primeira atividade

---
+++ Atividade_01.dart
@@ -0,0 +1,33 @@
+class Veiculo{
+  String cor;
+  int velocidade;
+  double peso;
+  Veiculo(this.cor, this.velocidade, this.peso);
+}
+
+class Carro extends Veiculo{
+  String buzina;
+  Carro(String cor, int velocidade, double peso, this.buzina): super(cor,velocidade,peso);
+}
+
+class Aviao extends Veiculo{
+  double tamanho;
+  Aviao(String cor, int velocidade, double peso, this.tamanho): super(cor,velocidade,peso);
+}
+
+void main(){
+  Carro carro1 = Carro('Azul',50,1200,'fom fom');
+  print(carro1.cor);
+  print(carro1.velocidade);
+  print(carro1.peso);
+  print(carro1.buzina);
+  print(' ');
+  Aviao aviao1 = Aviao('Branco',850,90,75);
+  print(aviao1.cor);
+  print(aviao1.velocidade);
+  print(aviao1.peso);
+  print(aviao1.tamanho);
+}
+
+  
+  
